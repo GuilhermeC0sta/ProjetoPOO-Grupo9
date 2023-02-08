46952784dbab8b8419319d42ba8bda3479f6222f
Micael <98262812+MicaelSv@users.noreply.github.com>
2023-02-08 18:47:59 -0300

    Update livro.java

--- livro.java
+++ livro.java
@@ -1,25 +1,18 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 //Função que registra um livro novo/retira um livro da biblioteca
 public class livro {
-    private String titulo;
-    private String autor;
-    private int isbn;
-    private int qnt_disp;
+    public String titulo;
+    public String autor;
+    public int isbn;
+    public int qnt_disp;
 
-    Scanner entrada = new Scanner(System.in);
+    public livro(String titulo, String autor, int isbn, int qnt_disp){
 
-    public livro(){
-        System.out.println("Insira o código isbn do livro: ");
-        this.isbn = entrada.nextInt();
-        System.out.println("Insira o título do livro: ");
-        this.titulo = entrada.nextLine();
-        System.out.println("Insira o autor do livro: ");
-        this.autor = entrada.nextLine();
-        System.out.println("Insira a quantidade disponível de cópias do livro no acervo: ");
-        this.qnt_disp = entrada.nextInt();
+        this.isbn = isbn;   
+        this.titulo = titulo;
+        this.autor = autor;
+        this.qnt_disp = qnt_disp;
+        
     }
 
 }
